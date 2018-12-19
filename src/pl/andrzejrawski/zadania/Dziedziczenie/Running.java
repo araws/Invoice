d14a16e2716856d555f017c4b284ceb5bd853b9f
araws <rawski.andrzej@gmail.com>
2018-12-19 10:56:23 +0100

    cokolwiek

---
+++ src/pl/andrzejrawski/zadania/Dziedziczenie/Running.java
@@ -0,0 +1,8 @@
+package pl.andrzejrawski.zadania.Dziedziczenie;
+
+public class Running extends Activity {
+    @Override
+    public void onCreate() {
+        System.out.println("onCreate");
+    }
+}
