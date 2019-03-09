64ed493d050874b46658a2894bf01722207269d9
araws <rawski.andrzej@gmail.com>
2019-03-09 20:22:56 +0100

    bigLetter

--- src/pl/andrzejrawski/zadania/zKsiazki/conditionalIstructions/GuessNumber.java
+++ src/pl/andrzejrawski/zadania/zKsiazki/conditionalIstructions/GuessNumber.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
 
-public class guessNumber {
+public class GuessNumber {
 
     public static void main(String[] args) throws IOException {
 
